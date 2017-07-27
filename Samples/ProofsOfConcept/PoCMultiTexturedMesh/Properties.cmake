39f36ab6d30fde215ede7e42039a588fe4bf506b
Alexandre Ancel <alexandre.ancel@ihu-strasbourg.eu>
2017-07-27 15:03:49 +0200

    feat(VTK): Add PoC to illustrate multi-texturing

---
+++ Samples/ProofsOfConcept/PoCMultiTexturedMesh/Properties.cmake
@@ -0,0 +1,24 @@
+set( NAME PoCMultiTexturedMesh )
+set( VERSION 0.1 )
+set( TYPE APP )
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+    appXml2
+    fwlauncher
+
+    echoSimulation
+
+    media
+
+    guiQt
+
+    io
+    ioVTK
+
+    uiIO
+
+    visuVTKQt
+    visuVTKAdaptor
+)
+
+bundleParam(appXml2 PARAM_LIST config PARAM_VALUES PoCMultiTexturedMeshConfig)
---
+++ Samples/ProofsOfConcept/PoCMultiTexturedMesh/README.md
@@ -0,0 +1,12 @@
+Proof of Concept: Multi-textured mesh
+=====================================
+
+This PoC allows to test multi-texturing in VTK via a simple application.
+
+Usage
+=====
+
+- Launch the application
+- Load a mesh via the "Load Mesh ..." icon in the toolbar
+- Load 3 images via the "Load Texture X ..." icons in the toolbar
+
