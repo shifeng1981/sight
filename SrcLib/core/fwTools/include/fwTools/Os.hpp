c75df1170fdf9190423e7db2d475705983b47f73
Marc Schweitzer <marc.schw@gmail.com>
2018-11-05 12:07:44 +0100

    feat(Os): linux fonction to find dev number from a device path (/dev/video#)

--- SrcLib/core/fwTools/include/fwTools/Os.hpp
+++ SrcLib/core/fwTools/include/fwTools/Os.hpp
@@ -1,16 +1,15 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
-#ifndef __FWTOOLS_OS_HPP__
-#define __FWTOOLS_OS_HPP__
-
-#include <string>
+#pragma once
 
 #include "fwTools/config.hpp"
 
+#include <string>
+#include <vector>
 
 namespace fwTools
 {
@@ -18,11 +17,9 @@ namespace fwTools
 /**
  * @brief   Namespace fwTools::os contains tools methods which depend on os like get user application data directory.
  *
- * @date    2009-2010.
  */
 namespace os
 {
-
 
 /**
  * @brief Returns a environment variable value
@@ -32,8 +29,7 @@ namespace os
  *
  * @return The environment variable content if it exists, else an empty string
  */
-FWTOOLS_API std::string getEnv(const std::string &name, bool *ok = NULL);
-
+FWTOOLS_API std::string getEnv(const std::string& name, bool* ok = NULL);
 
 /**
  * @brief Returns a environment variable value
@@ -41,7 +37,7 @@ FWTOOLS_API std::string getEnv(const std::string &name, bool *ok = NULL);
  * @param name environment variable 'name'
  * @param defaultValue Value returned if variable 'name' doesn't exist
  */
-FWTOOLS_API std::string getEnv(const std::string &name, const std::string &defaultValue);
+FWTOOLS_API std::string getEnv(const std::string& name, const std::string& defaultValue);
 
 /**
  * @brief   Return the users's application data directory
@@ -49,21 +45,29 @@ FWTOOLS_API std::string getEnv(const std::string &name, const std::string &defau
  * @param company The company name
  * @param appName The application name
  * @param createDirectory if true, create the returned directory if it don't exist
- * @date    2007-2009.
  *
  * Return the application data directory. If company or appName is not empty, append them to
  * the path. Under unix, XDG conventions are repected.
  * For example, the UserDataDir under linux will be "~/.config/company/appName"
  */
 FWTOOLS_API std::string  getUserDataDir(
-    std::string company = "",
-    std::string appName = "",
+    std::string company  = "",
+    std::string appName  = "",
     bool createDirectory = false
     );
 
+/**
+ * @brief getDeviceFromVirtualDevice returns the real path of a usb video device (Only Linux for now).
+ * @param _virtualDeviceUri: the virtual device uri (/dev/video# on linux, ...)
+ * @return std::vector< std::pair< int, std::string> >: vector of pair of device number (int) & system path (string)
+ *  of given virtual device uri.
+ * The vector is usualy of size 1, but if associated usb device is found  (such as depth sensor on a RGBD camera)
+ * the vector can be larger.
+ * vector[0] = usb device number and system path corresponding to given virtual device URI.
+ * vector[1-N] = all related usb device number and its system path.
+ */
+FWTOOLS_API std::vector< std::pair< int, std::string > > getDeviceFromVirtualDevice(std::string _virtualDeviceUri);
 
 } // namespace os
 
 } // namespace fwTools
-
-#endif //__FWTOOLS_OS_HPP__
--- SrcLib/core/fwTools/src/fwTools/Os.cpp
+++ SrcLib/core/fwTools/src/fwTools/Os.cpp
@@ -1,15 +1,18 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
-
 #include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
 
 #include "fwCore/base.hpp"
 #include "fwTools/Os.hpp"
 
+#ifdef __linux
+#include <sys/stat.h>
+#endif
 
 namespace fwTools
 {
@@ -17,9 +20,11 @@ namespace fwTools
 namespace os
 {
 
-std::string getEnv(const std::string &name, bool *ok)
+//------------------------------------------------------------------------------
+
+std::string getEnv(const std::string& name, bool* ok)
 {
-    char *value = std::getenv(name.c_str());
+    char* value = std::getenv(name.c_str());
     bool exists = (value != NULL);
     if(ok != NULL)
     {
@@ -30,7 +35,7 @@ std::string getEnv(const std::string &name, bool *ok)
 
 //------------------------------------------------------------------------------
 
-std::string getEnv(const std::string &name, const std::string &defaultValue)
+std::string getEnv(const std::string& name, const std::string& defaultValue)
 {
     bool ok           = false;
     std::string value = getEnv(name, &ok);
@@ -43,7 +48,7 @@ std::string getUserDataDir( std::string company, std::string appName, bool creat
 {
     std::string dataDir;
 #ifdef WIN32
-    char *appData = std::getenv("APPDATA");
+    char* appData = std::getenv("APPDATA");
     dataDir = ::fwTools::os::getEnv("APPDATA");
 #else
     bool hasXdgConfigHome     = false;
@@ -57,7 +62,6 @@ std::string getUserDataDir( std::string company, std::string appName, bool creat
     {
         dataDir += "/" + company;
     }
-
 
     if ( !appName.empty() )
     {
@@ -84,7 +88,124 @@ std::string getUserDataDir( std::string company, std::string appName, bool creat
     return dataDir;
 }
 
+//------------------------------------------------------------------------------
+
+std::vector< std::pair< int, std::string > >getDeviceFromVirtualDevice(std::string _devURI)
+{
+
+    std::vector< std::pair< int, std::string > > output;
+    output.push_back(std::make_pair(-1, "ERROR"));
+
+#ifdef __linux
+    // Use stat to get infos on device.
+    struct stat sdata;
+    int ret = stat(_devURI.c_str(), &sdata);
+
+    if(ret < 0)
+    {
+        SLM_ERROR("`" + _devURI + "` is not a valid /dev/video# path");
+        return output; // Default values with error code.
+    }
+
+    std::string subpath;
+
+    if (S_ISBLK(sdata.st_mode))
+    {
+        subpath = "block/";
+    }
+    else if (S_ISCHR(sdata.st_mode))
+    {
+        subpath = "char/";
+    }
+    else
+    {
+        SLM_ERROR("`" + _devURI + "` is not a valid /dev/video# path");
+        return output; // Default values with error code.
+    }
+
+    // Construct a path /sys/dev/{block,char}/<maj>:<min>
+    ::boost::filesystem::path path = "/sys/dev/" + subpath + std::to_string(major(sdata.st_rdev)) + ":"
+                                     + std::to_string(minor(sdata.st_rdev)) + "/device/../";
+
+    // Follow symlink
+    ::boost::filesystem::path realpath = ::boost::filesystem::canonical(path);
+
+    std::ifstream ifs(realpath.string() + "/devnum");
+    if(ifs.is_open())
+    {
+        std::string content( (std::istreambuf_iterator<char>(ifs) ),
+                             (std::istreambuf_iterator<char>()    ) );
+
+        // Change default values.
+        output[0].first  = std::stoi(content);
+        output[0].second = realpath.string();
+
+        ifs.close();
+    }
+    else
+    {
+        SLM_ERROR("'devnum' file cannot be read at: '" + realpath.string() + "'. The device number cannot be found for "
+                  + _devURI );
+        return output; // Default values with error code.
+    }
+
+    // Also check associated devnum (for RGBD cam for example).
+    // looking for paths like: /sys/devices/pci###/usb3/3.x/3.x.y
+    // All 3.x.y belongs to the same usb device.
+
+    // Go to parent directory and check if associated usb device exists. (3.x/)
+    ::boost::filesystem::path parentPath = realpath.parent_path();
+    ::boost::filesystem::path parentDir  = parentPath.filename();
+
+    // Get last character to exclude it from regex
+    // Looking for all directories named 3.x.y except the one already found.
+    const ::boost::regex my_filter( parentDir.string() + "\\.[^" + realpath.string().back() + "]" );
+
+    std::vector< std::string > all_matching_files;
+
+    boost::filesystem::directory_iterator end_itr; // Default ctor yields past-the-end
+    for( ::boost::filesystem::directory_iterator i( parentPath ); i != end_itr; ++i )
+    {
+        // Skip if not a directory
+        if( !boost::filesystem::is_directory( i->status() ) )
+        {
+            continue;
+        }
+
+        ::boost::smatch what;
+
+        //Skip if don't respect the regex
+        if( !::boost::regex_match( i->path().filename().string(), what, my_filter ) )
+        {
+            continue;
+        }
+
+        ::boost::filesystem::path associatedDevPath = i->path();
+
+        std::ifstream ifs(i->path().string() + "/devnum");
+        if(ifs.is_open())
+        {
+            std::string content( (std::istreambuf_iterator<char>(ifs) ),
+                                 (std::istreambuf_iterator<char>()    ) );
+
+            output.push_back(std::make_pair(std::stoi(content), i->path().string()));
+
+            ifs.close();
+        }
+        else
+        {
+            SLM_ERROR("'devnum' file cannot be read at: '" + associatedDevPath.string()
+                      + "'. No associated device were added" );
+        }
+    }
+
+#endif
+
+    return output;
+}
+
+//------------------------------------------------------------------------------
+
 } // namespace os
 
 } // namespace fwTools
-
