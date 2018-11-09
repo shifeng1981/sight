b9880dd3e360db73b30de63d8fea5dd4b4f4ddaa
Marc Schweitzer <marc.schw@gmail.com>
2018-11-09 17:18:12 +0100

    feat(astra): new utilities to record or visualise streams from Astra.

---
+++ Utilities/astra/astraRecord3Streams/Properties.cmake
@@ -0,0 +1,13 @@
+
+set( NAME astraRecord3Streams )
+set( VERSION 0.1 )
+set( TYPE EXECUTABLE )
+set( OPTIONS CONSOLE TRUE )
+set( DEPENDENCIES )
+set( REQUIREMENTS  )
+
+set( CONAN_DEPS
+    boost/1.67.0@sight/stable
+    opencv/3.4.3@sight/stable
+    openni/2.2.0-rev-958951f@sight/stable
+)
